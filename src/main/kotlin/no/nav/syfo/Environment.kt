0707262e44081897a87b822de53a733eaecf01fb
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-19 09:10:29 +0100

    skulle vært _ i når henter ut service bruker

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -21,8 +21,8 @@ private val config = Properties().apply {
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
-    val srvappnameUsername: String = getEnvVar("SRVOP-MODIA_USERNAME"),
-    val srvappnamePassword: String = getEnvVar("SRVOP-MODIA_PASSWORD"),
+    val srvappnameUsername: String = getEnvVar("SRVOP_MODIA_USERNAME"),
+    val srvappnamePassword: String = getEnvVar("SRVOP_MODIA_PASSWORD"),
     val kafkaBootstrapServersURL: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
 
     val kafkaIncommingTopicOppfolginsplan: String = config.getProperty("kafka.privat.syfo.oppfolingsplan.topic")
